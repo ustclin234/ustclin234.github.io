55f80a65c059cadb35f491671c3a0156c4da9ea8
ustclin <38360083+ustclin234@users.noreply.github.com>
2024-10-30 22:06:13 +0800

    2024-10-30-4

--- generate_json.py
+++ generate_json.py
@@ -7,6 +7,9 @@ def generate_json(file_folder, save_folder='./json_files/'):
         "images": files
     }
     js = json.dumps(contents, sort_keys=True,indent=4,separators=(',',':'),ensure_ascii=False)
+
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
     with open(f'{save_folder}qsshouban.json','w',newline='\n',encoding='utf-8') as file:
         file.write(js)
 
