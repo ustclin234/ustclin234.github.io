cca7f33e8546212b709b626fb3301b0c9f564ec8
ustclin <38360083+ustclin234@users.noreply.github.com>
2024-10-30 21:46:05 +0800

    2024-10-30-2

---
+++ generate_json.py
@@ -0,0 +1,14 @@
+# coding = utf-8
+import json,os
+
+def generate_json(file_folder, save_folder='./json_files/'):
+    files = sorted([s for s in os.listdir(file_folder) if s.endswith('png')])
+    contents = {
+        "images": files
+    }
+    js = json.dumps(contents, sort_keys=True,indent=4,separators=(',',':'),ensure_ascii=False)
+    with open(f'{save_folder}qsshouban.json','w',newline='\n',encoding='utf-8') as file:
+        file.write(js)
+
+file_folders = './qsshouban/'
+generate_json(file_folders)
