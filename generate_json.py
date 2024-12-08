90c44689a833dbb1f81cc8235cf1de1a5b49ad05
ustclin <38360083+ustclin234@users.noreply.github.com>
2024-12-08 19:00:14 +0800

    2024-12-06-1

--- generate_json.py
+++ generate_json.py
@@ -13,7 +13,19 @@ def generate_json(file_folder, item='qsshouban', save_folder='./json_files/'):
     with open(f'{save_folder}{item}.json','w',newline='\n',encoding='utf-8') as file:
         file.write(js)
 
+def generate_pdf_json(file_folder, item='qsshouban2',save_folder='./json_files/'):
+    files = sorted([s for s in os.listdir(file_folder) if s.endswith('.pdf')])
+    contents = {
+        "pdfs": files
+    }
+    js = json.dumps(contents, sort_keys=True,indent=4,separators=(',',':'),ensure_ascii=False)
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+    with open(f'{save_folder}{item}.json','w',newline='\n',encoding='utf-8') as file:
+        file.write(js)
+
 file_folders1 = './qsshouban/'
 file_folders2 = './qsgupu/'
 generate_json(file_folders1, 'qsshouban')
 generate_json(file_folders2, 'qsgupu')
+generate_pdf_json('./qsshouban2/', 'qsshouban2')
